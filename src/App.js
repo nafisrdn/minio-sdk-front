a2ae48d2a45f1c8779cb464b380198c1bd708b15
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2022-07-15 13:40:31 +0700

    Commit
    App ready to use for testing purpose

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
+import { API_HOST } from './config'
 import './App.css';
+import { bytesToSize } from './utils';
 
 function App() {
+  const [file, setFile] = useState('')
+  const [actionHistory, setActionHistory] = useState([])
+  const inputFileRef = useRef()
+
+  const [isLoading, setIsloading] = useState(false)
+
+  useEffect(() => {
+    console.log(actionHistory)
+  }, [actionHistory])
+
+  const formHandle = async e => {
+    e.preventDefault()
+
+    if (!file)
+      return
+
+    try {
+      setIsloading(true)
+      console.log(file);
+      const formData = new FormData();
+      formData.append('file', file)
+
+      const res = await axios.post(API_HOST + 'terserahlah', formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+
+      setActionHistory(state => [...state, { isSuccess: true, fileName: file.name,...res.data }])
+    } catch (error) {
+      console.error(error)
+      setActionHistory(state => [...state, {isSuccess: false, fileName: file.name, ...error.response.data}])
+    }
+
+    inputFileRef.current.value = ''
+    setFile(null)
+
+    setIsloading(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      <h1 className='text-center mt-5'>Upload MinIO</h1>
+      <form onSubmit={formHandle}>
+        <div className="mb-3">
+          <label htmlFor="file" className="form-label">File:</label>
+          <input type='file' name='file' className='form-control' id='file' onChange={e => setFile(e.target.files[0])} ref={inputFileRef}/>
+        </div>
+        {file && 
+          <div className="mb-3">
+            <span>File Size: {bytesToSize(file.size)}</span>
+          </div>
+        }
+        {!isLoading ? <button className='btn btn-primary'>Upload</button> : <button className='btn btn-secondary' disabled>Loading...</button>}
+        
+      </form>
+
+      <ul className='action-history mt-5'>
+        {actionHistory.map((value, index) => (
+          <li key={value.etag}>
+            <div className={`alert ${value.isSuccess ? 'alert-success' : 'alert-danger'}`}>
+              {index + 1}. {value.message}: {value.fileName}
+            </div> 
+          </li>))}
+      </ul>
     </div>
   );
 }
