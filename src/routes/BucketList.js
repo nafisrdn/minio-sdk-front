b40e0787eedf1375827d3cd0e8795b82aebd3aa1
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2022-07-23 12:26:23 +0700

    Update bucket list page ui

--- src/routes/BucketList.js
+++
@@ -1,9 +0,0 @@
-import MainLayout from "../components/layouts/MainLayout";
-
-export default function BucketList() {
-  return (
-    <MainLayout>
-      <h1>list</h1>
-    </MainLayout>
-  );
-}
---
+++ src/routes/BucketList.jsx
@@ -0,0 +1,18 @@
+import MainLayout from "../components/layouts/MainLayout";
+import { Link } from "react-router-dom";
+
+
+export default function BucketList() {
+  return (
+    <MainLayout>
+      <h1 className="mb-5">Bucket List</h1>
+
+      <div className="list-group">
+        <Link to='/bucket/test' className="list-group-item list-group-item-action">My Bucket</Link>
+        <Link to='a' className="list-group-item list-group-item-action">Second Bucket</Link>
+        <Link to='a' className="list-group-item list-group-item-action">Final Bucket</Link>
+
+      </div>
+    </MainLayout>
+  );
+}
